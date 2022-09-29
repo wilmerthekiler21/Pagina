bb5bf72c6fedf4c23094d220e8d5ec24ad1b9613
wilmerthekiler21 <95238585+wilmerthekiler21@users.noreply.github.com>
2022-09-29 11:08:25 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="reset.css">
+</head>
+<body>
+ <header class="main-hader">
+        <nav class="main-nav">
+            <div class="logo">
+                <span>Loquendo</span>
+            </div>
+            <ul>
+                <li><a href="#">NA</a></li>
+                <li><a href="#">Matematicas</a></li>
+                <li><a href="#">Ingles</a></li>
+                <li><a href="#">Programacion</a></li>
+            </ul>
+        </nav>
+        <div class="content-header">
+           <h1>Tema Uno</h1> 
+           <p>Resumen de Seguridad de informacion</p>
+            <a href="#">Ver informacion</a>
+        </div>
+ </header>
+</body>
+</html>
